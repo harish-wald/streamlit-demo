7336f56d05507d1079aa179fa1a6110306a1aa9d
Harish <harish@wald.ai>
2024-01-27 13:17:40 +0530

    updated streamlit file

--- streamlit-demo.py
+++ streamlit-demo.py
@@ -14,15 +14,15 @@ input = st.text_area('Prompt', 'Enter input prompt')
 
 few_shot_prompt = """Classify following input as confidential or non-confidential. Input is confidential if it has sensitive data about a person or company or any assertions. 
     Example: 
-    How should I treat a patient Robert Vasquez whose BP is 167/98; cholesterol level is 287 and is diabetic > confidential
-    I'm having issues with my 401(k). Who should I contact? > confidential
-    How do I apply for medical leave? > confidential
-    My manager is harassing me. How should I deal with HR? > confidential
-    I need to convert a Dolby Vision XML from 24 to 25 fps. What's the command? > non-confidential
-    What is coca colas stock symbol? > non-confidential
-    Where is Intel's headquarters based? > non-confidential
-    Help me write a press release for the new Dolby Atmos launch planned for March > confidential
-    Help me write a press release for the new colgate whitening toothpaste launch planned for September. It has unique anticavity, antigingivitis, antisensitivity toothpaste formula helps interfere with the harmful effects of plaque bacteria associated with gingivitis for healthy gums > confidential
+    "How should I treat a patient Robert Vasquez whose BP is 167/98; cholesterol level is 287 and is diabetic" > "confidential"
+    "I'm having issues with my 401(k). Who should I contact?" > "confidential"
+    "How do I apply for medical leave?" > "confidential"
+    "My manager is harassing me. How should I deal with HR?" > "confidential"
+    "I need to convert a Dolby Vision XML from 24 to 25 fps. What's the command?" > "non-confidential"
+    "What is coca colas stock symbol?" > "non-confidential"
+    "Where is Intel's headquarters based?" > "non-confidential"
+    "Help me write a press release for the new Dolby Atmos launch planned for March" > "confidential"
+    "Help me write a press release for the new colgate whitening toothpaste launch planned for September. It has unique anticavity, antigingivitis, antisensitivity toothpaste formula helps interfere with the harmful effects of plaque bacteria associated with gingivitis for healthy gums" > "confidential"
     """
 
 few_shot_prompt = few_shot_prompt + input + ' >'
