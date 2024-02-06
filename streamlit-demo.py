6e576e214b8fd6c939369cff348b8773b62c578f
harish-wald <157676308+harish-wald@users.noreply.github.com>
2024-02-05 21:22:44 -0500

    Update streamlit-demo.py

--- streamlit-demo.py
+++ streamlit-demo.py
@@ -1,47 +1,39 @@
-
 import streamlit as st
+from transformers import pipeline
+import torch 
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoTokenizer
 
-import openai
-openai.api_key = st.secrets["OPENAI_API_KEY"]
-
-from openai import OpenAI
-client = OpenAI()
-
-
-st.title('Sensitive Prompt Detection')
+st.title('Sensitive Prompt Detection - Finetuned model')
 
 input = st.text_area('Prompt', 'Enter input prompt')
 
-few_shot_prompt = """Classify following input as confidential or non-confidential. Input is confidential if it has sensitive data or non public knowledge about a person or company. 
-    Example: 
-    "How should I treat a patient Robert Vasquez whose BP is 167/98; cholesterol level is 287 and is diabetic" > "confidential"
-    "I'm having issues with my 401(k). Who should I contact?" > "confidential"
-    "How do I apply for medical leave?" > "confidential"
-    "My manager is harassing me. How should I deal with HR?" > "confidential"
-    "I need to convert a Dolby Vision XML from 24 to 25 fps. What's the command?" > "non-confidential"
-    "What is coca colas stock symbol?" > "non-confidential"
-    "Where is Intel's headquarters based?" > "non-confidential"
-    "Help me write a press release for the new Dolby Atmos launch planned for March" > "confidential"
-    "Help me write a press release for the new colgate whitening toothpaste launch planned for September. It has unique anticavity, antigingivitis, antisensitivity toothpaste formula helps interfere with the harmful effects of plaque bacteria associated with gingivitis for healthy gums" > "confidential"
-    "i have frequent headaches. what should i do." > "non-confidential"
-    "Is it okay for me to attack Diet Coke in an ad campaign?" > "non-confidential"
-    "is apple ceo gay" > "non-confidential"
-    "is my boss elon musk married" > "non-confidential"
-    """
+# Load the model (only executed once!)
+# NOTE: Don't set ttl or max_entries in this case
+@st.cache_resource
+def load_model():
+	  return AutoModelForSequenceClassification.from_pretrained("Harish-wald/sensitive-bert",token="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", num_labels=2)
 
-few_shot_prompt = few_shot_prompt + '\n"'+ input + '" >'
+@st.cache_resource
+def load_tokenizer():
+	  return AutoTokenizer.from_pretrained("bert-base-cased")
 
+model = load_model()
+
+model.eval()
+
+tokenizer = load_tokenizer()
 
 def run_prompt():
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo-1106",
-        messages=[
-            {"role": "system", "content": ""},
-            {"role": "user", "content": few_shot_prompt}
-            ]
-            )
+    tokenized_input = tokenizer([input], padding="max_length", truncation=True,return_tensors='pt')
+    output = model.forward(**tokenized_input)
     st.subheader('Output: ')
-    st.write(completion.choices[0].message.content)
-st.button('Run', on_click=run_prompt)
+    label = torch.argmax(output['logits']).tolist()
+    output = 'non-confidential'
+    if label:
+        output = 'confidential'
 
+    st.write(output)
+
+st.button('Run', on_click=run_prompt)
 
