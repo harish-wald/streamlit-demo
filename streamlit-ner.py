ad8108da8b630e6bd6d058a7a5fe37318246a1f2
harish-wald <harish@wald.ai>
2024-03-15 03:21:53 -0400

    updating to latest ner model

--- streamlit-ner.py
+++ streamlit-ner.py
@@ -1,77 +1,24 @@
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 from transformers import pipeline
 import streamlit as st
+from gliner import GLiNER
 
-@st.experimental_singleton
+@st.cache_resource
 def load_model():
-    tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
-    model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
-    return pipeline("ner", model=model, tokenizer=tokenizer)
-
+    return GLiNER.from_pretrained("urchade/gliner_largev2")
 
 
 st.title('Named entity detection')
 
 example = st.text_area('Prompt', 'Enter input prompt')
 
+labels = ["person", "award", "date", "competitions", "teams", "company"]
+
 def run_prompt():
-    nlp = load_model()
-    ner_results = nlp(example)
+    model = load_model()
+    entities = model.predict_entities(example, labels)
 
-    result_map = {}
-    i =0
-    j=1
-    merged_list=[]
-    print(ner_results)
-    while j < len(ner_results):
-        print(ner_results[j])
-        person = ('PER' in ner_results[j]['entity'] and 'PER' in ner_results[j-1]['entity'])
-        org = ('ORG' in ner_results[j]['entity'] and 'ORG' in ner_results[j-1]['entity'])
-        print(person)
-        print(org)
-        if ner_results[j]["start"] == ner_results[j-1]["end"] and (person or org):
-            j+=1
-        else:
-            result ={}
-            result["start"]=ner_results[i]['start']
-            result["end"]=ner_results[j-1]['end']
-            result["word"] = example[result["start"]:result["end"]]
-            person = ('PER' in ner_results[j-1]['entity'])
-            org = ('ORG' in ner_results[j-1]['entity'])
-            print(result)
-            if person:
-                result['entity']='person'
-            if org:
-                result['entity']='company'
-            merged_list.append(result)
-            i=j
-            j+=1
-
-    if j==len(ner_results):
-        person = 'PER' in ner_results[j-1]['entity']
-        org = 'ORG' in ner_results[j-1]['entity'] 
-        result ={}
-        result["start"]=ner_results[i]['start']
-        result["end"]=ner_results[j-1]['end']
-        result["word"] = example[result["start"]:result["end"]]
-        if person:
-             result['entity']='person'
-        if org:
-            result['entity']='company'
-        merged_list.append(result)
-
-    for result in merged_list:
-        if 'entity' not in result:
-            continue
-        print(result)
-        word = result["word"]
-        print(word)
-        result_map[word] = result["entity"]
-
-    st.subheader('Output: ')
-    print(result_map)
-    st.write(result_map)
+    for entity in entities:
+          st.write(entity["text"], "=>", entity["label"])
 
 st.button('Run', on_click=run_prompt)
-
-
